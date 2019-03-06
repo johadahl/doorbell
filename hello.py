60d448a8d665a2d6de6a8b69c018e418b49627aa
Johan Dahl <johad@kth.se>
2019-03-06 22:40:40 +0100

    added twilio response

--- hello.py
+++ hello.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from twilio.twiml.voice_response import VoiceResponse, Dial, Play
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello world'
+
+@app.route("/answer", methods=['GET', 'POST'])
+def answer_call():
+    """Respond to incoming phone calls with a brief message."""
+    # Start our TwiML response
+    resp = VoiceResponse()
+
+    # Read a message aloud to the caller
+    resp.say("Välkommen upp till femte våningen", voice='alice', language='sv-SE')
+    resp.play('', digits='w*')
+
+    return str(resp)
+
 
 if __name__ == '__main__':
     app.run()
