3ca1602ae6406261876098be5e652b4c3e40b0c5
Johan Dahl <johad@kth.se>
2019-03-06 23:06:17 +0100

    added not home response

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ from flask import Flask
 from twilio.twiml.voice_response import VoiceResponse, Dial, Play
 
 app = Flask(__name__)
+home = False
 
 @app.route('/')
 def hello_world():
@@ -14,10 +15,12 @@ def answer_call():
     resp = VoiceResponse()
 
     # Read a message aloud to the caller
-    resp.say("Welcome. Proceed to the fifth floor", voice='alice', language='en-EN')
-    resp.play('', digits='w*')
+    if home:
+	    resp.say("Welcome. Proceed to the fifth floor", voice='alice', language='en-EN')
+	    resp.play('', digits='w*')
+    else:
+    	resp.say("Hello. Noone is home right now. Please call 0 7 0 7 4 4 7 4 1 3 if you need to be let inside. Thank you", voice='alice', language='en-EN')
     return str(resp)
-
 
 if __name__ == '__main__':
     app.run()
